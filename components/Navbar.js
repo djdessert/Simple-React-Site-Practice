612f28f1afbfd36169795acc0546f51b2bd7db8a
Angela Taylor <djdessert@gmail.com>
2023-04-25 17:08:39 +0700

    initial push from vs code

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <div className ="Navbar">
+            <img src="./images/reactjs-icon.png"></img>
+            <h1> ReactFacts</h1>
+            <p>React Course - Project 1 </p>
+
+        </div>
+    )
+}
